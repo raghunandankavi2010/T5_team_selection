25ea28cb405f457338596a35f8ac4e13d70c1501
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2019-12-06 00:31:30 +0530

    HomeActivity for user to say yes or no.

---
+++ Tiger5Regulars/app/src/main/java/com/example/raghu/tiger5regulars/HomeActivity.kt
@@ -0,0 +1,94 @@
+package com.example.raghu.tiger5regulars
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.opengl.Visibility
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_home.*
+import java.util.*
+
+
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+    private val PREF_NAME = "login"
+    private var PRIVATE_MODE = 0
+    private var sharedPref: SharedPreferences? = null
+    private var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if(account==null) {
+            val intent = Intent(this@HomeActivity,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else {
+            setContentView(R.layout.activity_home)
+            val sharedPref: SharedPreferences = getSharedPreferences(PREF_NAME, PRIVATE_MODE)
+            database = FirebaseDatabase.getInstance().reference
+            checkCount()
+            switch_btn.visibility = View.GONE
+            switch_btn.setOnCheckedChangeListener { _, isChecked ->
+                if(count in 1..10){
+                    update(sharedPref.getString(PREF_NAME,null),isChecked,sharedPref.getString("id",null))
+                }else{
+                    if(!isChecked) {
+                        switch_btn.isChecked = false
+                        update(sharedPref.getString(PREF_NAME,null),isChecked,sharedPref.getString("id",null))
+
+                    }else if(isChecked && count==0){
+                        update(sharedPref.getString(PREF_NAME,null),isChecked,sharedPref.getString("id",null))
+
+                    }else{
+                        Snackbar.make(root, "Cannot include already 10 people are in", Snackbar.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun update(name:String?,playing:Boolean,uid:String?){
+
+
+        val objRef = database.child("Players")
+        uid?.let {
+         objRef.child(it).child("Playing").setValue(playing)
+            startActivity(Intent(this@HomeActivity,MainActivity::class.java))
+        }
+    }
+
+    private fun checkCount() {
+        val ref = database.child("Players")
+        val playersQuery = ref.orderByChild("Playing").equalTo(true)
+
+
+        playersQuery.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var count = 0
+                    for (players in dataSnapshot.children) {
+                        count++
+                    }
+                if(count in 1..10 || count==0){
+                    switch_btn.visibility = View.VISIBLE
+                }
+                }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("HomeActivity", "loadPost:onCancelled", databaseError.toException())
+            }
+        })
+    }
+
+    private fun getCurrentDateTime(): Date {
+        return Calendar.getInstance().time
+    }
+
+}
